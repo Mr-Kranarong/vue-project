2615ba22f6b79eba6aa327d6856a982aeb8bce26
God Emperor Kranarong <Mr.Kranarong@Gmail.Com>
2020-05-26 17:23:00 +0700

    Create components and shits

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoPage/>
+    <!--<task-list></task-list>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import ToDoPage from './pages/ToDoPage.vue'
+//import TaskList from './components/TaskList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoPage
   }
 }
 </script>
@@ -24,5 +28,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app>img{
+  transition: .8s;
+}
+#app>img:hover{
+  transform: rotate(360deg);
+  transition: .8s;
 }
 </style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="container">
+        <div class="task-zone">
+            <div class="drop-zone">
+                <h1>What Need To Be Done</h1>
+                <div class="drag-el" v-for="task in needToBeDone" :key="task.id">{{task.title}}</div>
+            </div>
+            <div class="drop-zone">
+                <h1>Das Kapital</h1>
+                <div class="drag-el" v-for="task in beingDone" :key="task.id">{{task.title}}</div>
+            </div>
+            <div class="drop-zone">
+                <h1>What Has Been Done</h1>
+                <div class="drag-el" v-for="task in hasBeenDone" :key="task.id">{{task.title}}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TaskList',
+    data(){
+        return{
+            tasks:[
+                {
+                    id: 1,
+                    title: 'Item A',
+                    status: 'HAS BEEN DONE'
+                },
+                {
+                    id: 2,
+                    title: 'Item B',
+                    status: 'NEED TO BE DONE'
+                },
+                {
+                    id: 3,
+                    title: 'Item C',
+                    status: 'BEING DONE'
+                },
+                {
+                    id: 4,
+                    title: 'Item D',
+                    status: 'HAS BEEN DONE'
+                },
+                {
+                    id: 5,
+                    title: 'Item E',
+                    status: 'NEED TO BE DONE'
+                },
+                {
+                    id: 6,
+                    title: 'Item F',
+                    status: 'BEING DONE'
+                },
+                {
+                    id: 7,
+                    title: 'Item G',
+                    status: 'HAS BEEN DONE'
+                },
+                {
+                    id: 8,
+                    title: 'Item H',
+                    status: 'NEED TO BE DONE'
+                },
+                {
+                    id: 9,
+                    title: 'Item I',
+                    status: 'BEING DONE'
+                }
+            ]
+        }
+    },
+    computed:{
+        needToBeDone(){
+            return this.tasks.filter(x => x.status == "NEED TO BE DONE")
+        },
+        beingDone(){
+            return this.tasks.filter(x => x.status == "BEING DONE")
+        },
+        hasBeenDone(){
+            return this.tasks.filter(x => x.status == "HAS BEEN DONE")
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container{
+    margin: 30px 0;
+}
+.task-zone{
+    display: flex;
+    justify-content: space-around;
+}
+.drop-zone{
+    border: 1px solid black;
+    width: 100%;
+    height: 400px;
+    margin: 0 30px;
+    border-radius: 20px;
+    padding: 10px 0;
+    transition: .3s;
+    background-color:white;
+}
+.drop-zone:hover{
+    transform: rotate(5deg);
+}
+.drag-el{
+    border: 1px solid black;
+    width: 200px;
+    height: 40px;
+    margin: 5px auto;
+    padding-top: 15px;
+    border-radius: 10px;
+    transition: .3s;
+    background-color:white;
+}
+.drag-el:hover{
+    border-radius: 30px;
+    transition: .3s;
+    transform: scale(1.2) rotate(-5deg);
+}
+</style>
